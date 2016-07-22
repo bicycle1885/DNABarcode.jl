ea9f8e31a9f722d3d0165665905d825a004c00d8
Kenta Sato <bicycle1885@gmail.com>
2016-07-22 21:45:06 +0900

    add filter!

--- src/abstractcode.jl
+++ src/abstractcode.jl
@@ -1,6 +1,7 @@
 # Abstract Code
 # =============
 
+# The existence of .codeword::Vector{DNAKmer{k}} is assumed.
 abstract AbstractCode{k} <: AbstractVector{DNAKmer{k}}
 
 function Base.getindex(code::AbstractCode, i::Integer)
@@ -9,4 +10,15 @@ end
 
 function Base.size(code::AbstractCode)
     return size(code.codewords)
+end
+
+function Base.filter!{k}(f::Function, code::AbstractCode{k})
+    filtered = DNAKmer{k}[]
+    for x in code
+        if f(x)
+            push!(filtered, x)
+        end
+    end
+    code.codewords = filtered
+    return code
 end
