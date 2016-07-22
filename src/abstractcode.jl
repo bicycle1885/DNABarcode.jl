0222aa278e7c2fababf80dec1b0d716a4700cb8a
Kenta Sato <bicycle1885@gmail.com>
2016-07-22 19:04:00 +0900

    fix size of AbstractCode

--- src/abstractcode.jl
+++ src/abstractcode.jl
@@ -7,6 +7,6 @@ function Base.getindex(code::AbstractCode, i::Integer)
     return code.codewords[i]
 end
 
-function Base.length(code::AbstractCode)
-    return length(code.codewords)
+function Base.size(code::AbstractCode)
+    return size(code.codewords)
 end
